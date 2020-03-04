2a371b82201f8d5b217d7ff4d39bd5076cdbf6c4
Sunny Lee <leesunny507@gmail.com>
2020-03-04 10:43:18 -0500

    Updates

---
+++ precache-manifest.9aac0df78f9418055ecbbba69b95efcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece630d0f0bbef66b56ded3faae5e2ee",
+    "url": "/GoogleBookSearch2020/index.html"
+  },
+  {
+    "revision": "6feac991edf5ef079735",
+    "url": "/GoogleBookSearch2020/static/css/main.29acd965.chunk.css"
+  },
+  {
+    "revision": "228d904734da981560ec",
+    "url": "/GoogleBookSearch2020/static/js/2.d16a2fdb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/GoogleBookSearch2020/static/js/2.d16a2fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6feac991edf5ef079735",
+    "url": "/GoogleBookSearch2020/static/js/main.97b42955.chunk.js"
+  },
+  {
+    "revision": "cd8a7a866b310287517b",
+    "url": "/GoogleBookSearch2020/static/js/runtime-main.b2e148d7.js"
+  }
+]);
